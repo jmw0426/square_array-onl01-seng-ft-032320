c2275edaafa2facaa7f907fabcc0d3566d523a29
Joshua Williams <jmw0426@hotmail.com>
2020-03-25 21:13:39 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,16 @@
 def square_array(array)
-  # your code here
-end
+  
+  numbers = []
+  
+  square_array(numbers)
+  
+  numbers.each do |square|
+    
+     numbers << square **2
+   
+  end    
+   
+  numbers
+  
+end  
+   
